c228b9732e8cd2f91e9a2dfe1201f7769f0e038e
Goven <1021073979@qq.com>
2020-03-04 15:33:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #=================================================
 # Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: Goven
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
