05e2a7e3289fe8fd286bb05056031fdf8c4513dc
Goven <1021073979@qq.com>
2020-03-05 10:22:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -4,4 +4,4 @@
 # Author: Goven
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
